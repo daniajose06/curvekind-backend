4dc0f2cf6a71a01ae1154a00aa878862f8c37b3c
Dania Jose <daniajose06@gmail.com>
2026-01-06 23:25:21 +0400

    Initial CurveKind Spring Boot backend (Gradle, Java 17)

---
+++ src/main/java/com/curvekind/backend/CurvekindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.curvekind.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurvekindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurvekindBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curvekind/backend/CurvekindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curvekind.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurvekindBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
